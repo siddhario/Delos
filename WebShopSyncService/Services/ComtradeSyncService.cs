77c730aeb1f4b37b1a9ae5122b4592aafeac6546
siddhario <dariodjekic@gmail.com>
2020-10-06 21:41:54 +0200

    Update ComtradeSyncService.cs

--- WebShopSyncService/Services/ComtradeSyncService.cs
+++ WebShopSyncService/Services/ComtradeSyncService.cs
@@ -20,13 +20,18 @@ namespace Delos.Services
             var result = await cl.GetCTProducts_WithAttributesAsync(Config.Username, Config.Password, null, null, null).ConfigureAwait(false);
             foreach (var res in result.Body.GetCTProducts_WithAttributesResult)
             {
+               
                 artikal a = new artikal() {prioritet = this.Config.Priority };
                 a.dobavljac_sifra = res.CODE;
                 a.dobavljac = this.Config.Description;
                 a.sifra = this.Config.Id.ToString().PadLeft(3, '0') + "_" + a.dobavljac_sifra;
                 a.naziv = res.NAME;
+                if (a.sifra == "002_NV70K1340BB/OL")
+                {
+                    int tt = 0;
+                }
                 decimal cijena;
-                decimal.TryParse(res.PRICENOTAX.Replace(",", "."), System.Globalization.NumberStyles.Any, new CultureInfo("en-US"), out cijena);
+                decimal.TryParse(res.PRICE.Replace(",", "."), System.Globalization.NumberStyles.Any, new CultureInfo("en-US"), out cijena);
                 a.cijena_sa_rabatom = cijena;
                 a.dostupnost = res.QTTYINSTOCK;
                 a.slike = res.IMAGE_URLS;
